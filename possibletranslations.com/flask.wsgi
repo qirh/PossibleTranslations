106c18ff55df59446664fbddd3b1e19c7015a636
saleh <salehg@live.com>
2018-07-05 19:35:49 -0400

    basic setup

---
+++ possibletranslations.com/flask.wsgi
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import sys
+import logging
+
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0 , "/var/www/pt/possibletranslations.com")
+
+from sala import app as application
+application.secret_key = "bL4jhC2rmk67lZmaqmkG8OozJztNbABPZ3zeaVUy2twdSTOsFe6UTKgytF0H5Z5YRAJlaIFw8YdYFg8g"
---
+++ possibletranslations.com/pt/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.errorhandler(404)
+def error_page(error1=None, error2=None, error3=None):
+    return render_template('/404.html')
+
+
+@app.route('/')
+@app.route ('/index')
+def index():
+    return render_template('/index.html')
