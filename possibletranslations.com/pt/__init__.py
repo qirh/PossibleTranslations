c5f9c65d2dbb5c9b806cec86dc010fc83f295eb0
saleh <salehg@live.com>
2018-07-08 15:05:50 -0400

    added config file

--- possibletranslations.com/pt/__init__.py
+++ possibletranslations.com/pt/__init__.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-from langdetect import detect
+from textblob import TextBlob
+from langdetect import detect, detect_langs, DetectorFactory
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///words.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config.from_object('config.Development')
 db = SQLAlchemy(app)
 
+def get_langs(word):
+
+    """
+    returns a list of the 3 most probable languages. the last two elemnts might be None
+    TextBlob return 1 language. langdetect return a list of languages.
+
+    if TextBlob == langdetect[0]
+        languages = langdetect[0:3]
+    else:
+        languages = TextBlob + langdetect[0:2]
+    """
+
+    first_lang = TextBlob(word).detect_language()
+    print("first_lang:")
+
+    langs = detect_langs(word)
+    langs = [w.lang.__str__() for w in langs]
+
+    if(first_lang.__str__() != langs[0]):
+        print("1")
+        langs.insert(0, first_lang.__str__())
+
+    # pad list if there are less than 3 elems
+    while (len(langs) < 4):
+        langs.append(None)
+    # remove last elements there are more than 3 elems
+    while (len(langs) > 3):
+        langs = langs[:-1]
+
+    return langs
 
 @app.errorhandler(404)
 def error_page(error1=None, error2=None, error3=None):
@@ -21,9 +50,10 @@ def error_page(error1=None, error2=None, error3=None):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
+    DetectorFactory.seed = 0
     if request.form:
-        print((request.form["word"]).decode('utf-8'))
-        print(detect(request.form["word"].decode('utf-8')))
+        print(get_langs(request.form["word"]))
+
     return render_template('/index.html')
 
 @app.route('/<name>')
--- possibletranslations.com/sql.sql
+++ possibletranslations.com/sql.sql
@@ -1,1 +1,14 @@
-CREATE TABLE flask_db.words (word_id BIGINT AUTO_INCREMENT, word VARCHAR(5000) NULL, language VARCHAR(1000) NULL, PRIMARY KEY (word_id));
+
+DROP TABLE if exists account_holder;
+
+
+CREATE TABLE flask_db.words (
+  word_id BIGINT AUTO_INCREMENT,
+  word VARCHAR(5000) NULL,
+  lang_1 VARCHAR(2) NULL,
+  lang_2 VARCHAR(2) NULL,
+  lang_3 VARCHAR(2) NULL,
+  translation_1 VARCHAR(5000) NULL,
+  translation_2 VARCHAR(5000) NULL,
+  translation_3 VARCHAR(5000) NULL,
+  PRIMARY KEY (word_id));
