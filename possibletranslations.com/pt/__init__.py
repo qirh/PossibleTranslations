a5237b4864ac16a373f3bd001922b4644a43fcc5
saleh <salehg@live.com>
2018-07-08 21:29:59 -0400

    removed models

--- possibletranslations.com/pt/__init__.py
+++ possibletranslations.com/pt/__init__.py
@@ -20,7 +20,6 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 TARGET = 'en'
 
-from pt.models import *
 
 def get_langs(word):
 
@@ -87,8 +86,41 @@ def index():
         print(translations)
         print(w.word_id)
         print(w)
+        db.session.add(w)
+        db.session.commit()
+    print("------------------")
+    words = Word.query.all()
+    print(words)
+    print("------------------")
     return render_template('/index.html')
 
 @app.route('/<name>')
 def hello_name(name):
     return "Hello {}!".format(name)
+
+
+
+class Word(db.Model):
+    __tablename__ = 'words'
+
+    word_id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(5000), index=True, unique=True, nullable=False)
+    lang_1 = db.Column(db.String(2), nullable=False)
+    lang_2 = db.Column(db.String(2))
+    lang_3 = db.Column(db.String(2))
+    translation_1 = db.Column(db.String(5000), nullable=False)
+    translation_2 = db.Column(db.String(5000))
+    translation_2 = db.Column(db.String(5000))
+
+
+    def __init__(self, word, lang_1, translation_1, lang_2=None, translation_2=None, lang_3=None, translation_3=None):
+        self.word = word
+        self.lang_1 = lang_1
+        self.lang_2 = lang_2
+        self.lang_3 = lang_3
+        self.translation_1 = translation_1
+        self.translation_2 = translation_2
+        self.translation_3 = translation_3
+
+    def __repr__(self):
+        return '<Word: {}. lang_1: {}. translation_1: {}>'.format(self.word, self.lang_1, self.translation_1)
