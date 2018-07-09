ff6e2a24d9ccdec711dfd28a04588ce139e08e17
saleh <salehg@live.com>
2018-07-08 23:46:55 -0400

    typo in one of the columns + started api

--- possibletranslations.com/pt/__init__.py
+++ possibletranslations.com/pt/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import exc
@@ -36,7 +36,7 @@ class WordTranslations(db.Model):
     lang_3 = db.Column(db.String(2))
     translation_1 = db.Column(db.String(5000), nullable=False)
     translation_2 = db.Column(db.String(5000))
-    translation_2 = db.Column(db.String(5000))
+    translation_3 = db.Column(db.String(5000))
 
 
     def __init__(self, word, target_lang, lang_1, translation_1, lang_2=None, translation_2=None, lang_3=None, translation_3=None):
@@ -51,6 +51,19 @@ class WordTranslations(db.Model):
 
     def __repr__(self):
         return '<Word: {}. lang_1: {}. translation_1: {}>'.format(self.word, self.lang_1, self.translation_1)
+
+    def serialize(self):
+        return {
+            'word_id': self.word_id,
+            'word': self.word,
+            'target_lang': self.target_lang,
+            'lang_1': self.lang_1,
+            'lang_2': self.lang_2,
+            'lang_3': self.lang_3,
+            'translation_1': self.translation_1,
+            'translation_2': self.translation_2,
+            'translation_3': self.translation_3,
+        }
 
 
 db.create_all()
@@ -151,3 +164,7 @@ def button():
     print(WordTranslations.query.all())
     return redirect('/')
 
+@app.route ('/api', methods=['GET'])
+def api():
+    words = WordTranslations.query.all()
+    return jsonify(eqtls=[e.serialize() for e in words])
