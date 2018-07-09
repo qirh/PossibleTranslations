47fec5c46a09b56d262169439507a412b822a5ac
saleh <salehg@live.com>
2018-07-08 20:13:04 -0400

    google api works

--- possibletranslations.com/pt/models.py
+++ possibletranslations.com/pt/models.py
@@ -15,8 +15,7 @@ class Word(db.Model):
     translation_2 = db.Column(db.String(5000))
 
 
-    def __init__(self, word_id, word, lang_1, translation_1, lang_2=None, translation_2=None, lang_3=None, translation_3=None):
-        self.word_id = word_id
+    def __init__(self, word, lang_1, translation_1, lang_2=None, translation_2=None, lang_3=None, translation_3=None):
         self.word = word
         self.lang_1 = lang_1
         self.lang_2 = lang_2
@@ -26,4 +25,4 @@ class Word(db.Model):
         self.translation_3 = translation_3
 
     def __repr__(self):
-        return '<Word {}>'.format(self.word)
+        return '<Word: {}>'.format(self.word)
