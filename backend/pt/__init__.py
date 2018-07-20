e6611435cd6540bd4b50018981ca4830264469f7
saleh <salehg@live.com>
2018-07-20 16:48:21 -0400

    spesrated models into another file

--- backend/pt/__init__.py
+++ backend/pt/__init__.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, request, render_template, jsonify, make_response
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS, cross_origin
 
@@ -10,6 +9,8 @@ from sqlalchemy import exc
 from textblob import TextBlob
 from langdetect import detect_langs, DetectorFactory
 from google.cloud import translate
+
+from models import db
 
 app = Flask(__name__)
 CORS(app)
@@ -20,55 +21,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://saleh:400700we@wordsaws.cl
 app.config['SQLALCHEMY_MIGRATE_REPO'] = 'db_repository'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
-db = SQLAlchemy(app)
+db.app = app
+db.init_app(app)
 migrate = Migrate(app, db)
 TARGET_LANGUAGE = 'en'
 AVALIABLE_LANGUAGES = []
-
-#########################################
-################# MODEL #################
-#########################################
-class WordTranslations(db.Model):
-    __tablename__ = 'words'
-    __table_args__ = (db.UniqueConstraint('word', 'target_lang'),)
-
-    word_id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(5000), nullable=False)
-    target_lang = db.Column(db.String(20), nullable=False)
-    lang_1 = db.Column(db.String(20), nullable=False)
-    lang_2 = db.Column(db.String(20), nullable=False)
-    lang_3 = db.Column(db.String(20), nullable=False)
-    translation_1 = db.Column(db.String(5000), nullable=False)
-    translation_2 = db.Column(db.String(5000), nullable=False)
-    translation_3 = db.Column(db.String(5000), nullable=False)
-
-
-    def __init__(self, word, target_lang, lang_1, translation_1, lang_2=None, translation_2=None, lang_3=None, translation_3=None):
-        self.word = word
-        self.target_lang = target_lang
-        self.lang_1 = lang_1
-        self.lang_2 = lang_2
-        self.lang_3 = lang_3
-        self.translation_1 = translation_1
-        self.translation_2 = translation_2
-        self.translation_3 = translation_3
-
-    def __repr__(self):
-        return '<Word: {}. target_lang: {}. lang_1: {}. translation_1: {}>'.format(self.word, self.target_lang, self.lang_1, self.translation_1)
-
-    def serialize(self):
-        return {
-            'word_id': self.word_id,
-            'word': self.word,
-            'target_lang': self.target_lang,
-            'lang_1': self.lang_1,
-            'lang_2': self.lang_2,
-            'lang_3': self.lang_3,
-            'translation_1': self.translation_1,
-            'translation_2': self.translation_2,
-            'translation_3': self.translation_3,
-        }
-
 
 def drop():
     db.reflect()
